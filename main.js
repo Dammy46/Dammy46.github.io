accdbee7a4d7cff23bb70bd7fc62963fa83e65bd
Dev Dammy <cranedammy46@gmail.com>
2022-04-01 18:59:14 +0100

    sections are completed

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-const typed = document.querySelector(".typed")
+
+const typed = document.querySelector('.typed');
 if (typed) {
-   let typed_strings = typed.getAttribute("data-typed-items")
-   typed_strings = typed_strings.split(",")
-   new Typed(".typed", {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 200
-   })
+  let typed_strings = typed.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 200,
+  });
 }
-   
+
 new PureCounter();
 
 new Swiper('.swiper', {
-   speed: 600,
-   loop: true,
-   autoPlay: {
-      delay: 4000,
-      disableOnInteraction: false
-   },
-   slidesPerView: 'auto',
-   pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-   },
-   breakpoints: {
+  speed: 600,
+  loop: true,
+  autoPlay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
       slidesPerView: 1,
-      spaceBetween: 20
-   },
-   1200: {
+      spaceBetween: 20,
+    },
+    1200: {
       slidesPerView: 3,
-      spaceBetween: 20
-   }
+      spaceBetween: 20,
+    },
+  },
 });
